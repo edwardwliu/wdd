8bd5bed8d68e4277900359b9d22170dad48f41f7
Edward Liu <edwardliu@airbears2-10-142-22-79.airbears2.1918.berkeley.edu>
2016-04-27 03:07:18 -0700

    added jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	
+	$("#list1").click(function(){
+		if($('#pixplored').css('display') == 'none'){ 
+   			$('#pixplored').show('slow'); 
+			} else { 
+   			$('#pixlored').hide('slow'); 
+		}
+	});
+
+	$("#list2").click(function(){
+		if($('#textd').css('display') == 'none'){ 
+   			$('#textd').show('slow'); 
+			} else { 
+   		$('#textd').attr('display', "none"); 
+		}
+	});
+
+	$("#list3").click(function(){
+		if($('#redditd').css('display') == 'none'){ 
+   			$('#redditd').show('slow'); 
+			} else { 
+   		$('#pixlored').hide('slow'); 
+		}
+	});
+
+	$(".year").hover(function() {
+  		$(".year-item").slideDown();
+	});
+
 });
-
-
